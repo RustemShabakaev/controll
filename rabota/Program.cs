15025d64c6792dbbaaa52b777460217f2737555c
rustem <prorusteg@gmail.com>
2022-12-25 18:23:25 +0300

    3

--- rabota/Program.cs
+++ rabota/Program.cs
@@ -4,3 +4,13 @@ string element = Console.ReadLine();
 string[] array = element.Split(' '); ;
 var result = new string[array.Length];
 var count = 0;
+foreach (var value in array)
+{
+
+    if (value.Length <= 3)
+    {
+        result[count] = value;
+        count++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, count));
